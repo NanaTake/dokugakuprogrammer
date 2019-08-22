699a5c1a257eaefbeb8fc3758435ba278806b825
NanaTake <son_magomago@yahoo.co.jp>
2019-08-22 23:18:39 +0900

    Create dp1-8.py
    組み込みモジュール

---
+++ dp1-8.py
@@ -0,0 +1,15 @@
+# mathという組み込みモジュールを使ってみる
+# pow(x, y) => x ** y
+import math
+math.pow(2, 3)
+
+# randomモジュールのrandint関数は、渡した引数の間で乱数を返す
+import random
+random.randint(0, 20)
+
+# statisticsモジュールは整数のリストから平均値、中央値、最頻値を算出できる
+nums = [1, 4, 24, 4, 13, 36, 2, 5]
+import statistics
+statistics.mean(nums)
+statistics.median(nums)
+statistics.mode(nums)
